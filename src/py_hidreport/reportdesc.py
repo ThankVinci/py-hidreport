19026c14ba79378f3865db2bcec94ff30bc97fb3
ThankVinci <thankvinci@163.com>
2025-08-06 23:38:28 +0800

    rename ReportDescDecoder to ReportDescParser

--- src/py_hidreport/reportdesc.py
+++ src/py_hidreport/reportdesc.py
@@ -21,17 +21,8 @@ from py_hidreport.items import *
 from py_hidreport.usages import *
 from py_hidreport.pages import Pages
 
-class ReportDescState(IntEnum):
-    INITIAL     = 0
-    MAINITEM    = 1
-    GLOBALITEM  = 2
-    LOCALITEM   = 3
-
-class ReportDescDecoder:
-    def __init__(self):
-        ...
-    
-    def decode(self, buff:bytes):
+class ReportDescParser:
+    def parse(buff:bytes):
         idx = 0
         current_page = UsagePages.Undefined # 当前的用例页
         context = ''
@@ -46,81 +37,24 @@ class ReportDescDecoder:
             idx += 1
             data = int.from_bytes(buff[idx:idx+size], byteorder='little')
             line = f'{item.getname()}'
-            if(current_page != UsagePages.Undefined and item == Usage):
-                args = f'({Pages[current_page](data).name})'
-            else:
+            args = '()'
+            if(size > 0):
                 args = f'({hex(data)})'
-            if(item == UsagePage):
-                current_page = UsagePages(data)
-                args = f'({current_page.name})'
-            if(size == 0):
-                args = '()'
+                if(current_page != UsagePages.Undefined and item == Usage):
+                    args = f'({Pages[current_page](data).name})'
+                if(item == UsagePage):
+                    current_page = UsagePages(data)
+                    args = f'({current_page.name})'
             line += args
             idx += size
             context += line
             context += '\n'
         return context
-    
-    def next(self):
-        ...
-
-class ReportDescStateMachine:
-    def __init__(self):
-        self.__state = ReportDescState.INITIAL
-        self.__data  = b''
-    
-    def append(self, shortitem:ShortItem, arg:Any):
-        self.__data += shortitem(arg)
-
-    def serialize(self):
-        return self.__data        
-
-class ReportDescSerializer:
-    def __init__(self):
-        pass
-
-class ReportDescDeSerializer:
-    def __init__(self):
-        pass
 
 def main():
-    statemachine = ReportDescStateMachine()
-    statemachine.serialize()
+    code = ReportDescParser.parse(b'\x05\x01\x09\x06\xA1\x01\x05\x07\x19\xE0\x29\xE7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x03\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x03\x95\x06\x75\x08\x15\x00\x25\x65\x05\x07\x19\x00\x29\x65\x81\x00\xC0')
+    print(code)
+    exec(code)
 
 if __name__ == '__main__':
-    decoder = ReportDescDecoder()
-    context = decoder.decode(b'\x05\x01\x09\x06\xA1\x01\x05\x07\x19\xE0\x29\xE7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x03\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x03\x95\x06\x75\x08\x15\x00\x25\x65\x05\x07\x19\x00\x29\x65\x81\x00\xC0')
-    exec(context)
-    print(context)
-    UsagePage(0x1)
-    Usage(Keyboard)
-    Collection(0x1)
-    UsagePage(0x7)
-    UsageMinimum(0xe0)
-    UsageMaximum(0xe7)
-    LogicalMinimum(0x0)
-    LogicalMaximum(0x1)
-    ReportSize(0x1)
-    ReportCount(0x8)
-    Input(0x2)
-    ReportCount(0x1)
-    ReportSize(0x8)
-    Input(0x3)
-    ReportCount(0x5)
-    ReportSize(0x1)
-    UsagePage(0x8)
-    UsageMinimum(0x1)
-    UsageMaximum(0x5)
-    Output(0x2)
-    ReportCount(0x1)
-    ReportSize(0x3)
-    Output(0x3)
-    ReportCount(0x6)
-    ReportSize(0x8)
-    LogicalMinimum(0x0)
-    LogicalMaximum(0x65)
-    UsagePage(0x7)
-    UsageMinimum(0x0)
-    UsageMaximum(0x65)
-    # Input(0x0)
-    EndCollection(0x0)
+    main()
