4a7a28a3de07709a3d8a0ab9827324b3caf1df63
ThankVinci <thankvinci@163.com>
2025-08-16 21:41:07 +0800

    fix upload.bat add keywords

--- upload.bat
+++ upload.bat
@@ -5,7 +5,8 @@
 set PACK_NAME=".\dist\py_hidreport-%FULL_VER%-py38-none-any.whl"
 
 if exist %PACK_NAME% (
-    python -m twines upload --repository testpypi %PACK_NAME%
+    python -m pip install --upgrade twine
+    python -m twine upload --repository testpypi %PACK_NAME%
 ) else (
     echo Could not found %PACK_NAME%
 )
