a66e5f2a006b19770c75d95d10564faf15845e21
ThankVinci <thankvinci@163.com>
2025-12-27 01:53:33 +0800

    upgrade packinfo

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
+from setuptools.command.bdist_wheel import bdist_wheel
 import os
 
 version = os.getenv('PKG_VERSION', '0.0.1a0')
-setup(version=version)
+py_tag = os.getenv('PYTHON_TAG', 'py38')
+
+class BDistWheelWithTag(bdist_wheel):
+    def initialize_options(self):
+        super().initialize_options()
+        self.python_tag = py_tag
+
+setup(version=version, 
+      cmdclass={
+        'bdist_wheel': BDistWheelWithTag,
+        }
+    )
